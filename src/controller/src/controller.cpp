1e5f1af99ba8b57732405bb567522f5a8dc99ecb
OffensiveNut <hafefun7377@gmail.com>
2024-11-20 18:56:57 +0700

    urdf sudah benar

--- src/controller/src/controller.cpp
+++ src/controller/src/controller.cpp
@@ -15,31 +15,27 @@ void moveto(int x, int y, int z)
 
 int main(int argc, char **argv)
 {
-    // Initialize ROS node
+// Initialize ROS node
     ros::init(argc, argv, "controller");
     ros::NodeHandle nh;
 
-    // Create a publisher object
+// Create a publisher object
     ros::Publisher pub = nh.advertise<std_msgs::Int32MultiArray>("servo", 10);
-
+    
     // Set the loop rate (10 Hz)
     ros::Rate loop_rate(100);
 
     int nomor = 0;
     while (ros::ok())
     {
-        if (nomor < 90)
-            nomor++;
-        else
-            nomor = 0;
         // Create and populate the message
-        std_msgs::Int32MultiArray msg;
-        msg.data = {nomor, 2, 3, 4, 5}; // Sample array of integers
+            std_msgs::Int32MultiArray msg;
+            msg.data = {0,0,0,0,0}; // Sample array of integers
 
         // Publish the message
-        pub.publish(msg);
-
-        ROS_INFO("Published array: [%d, 2, 3, 4, 5]",nomor);
+            pub.publish(msg);
+            
+        ROS_INFO("Published array: [0,0,0,0,0]");
 
         // Call any callbacks and sleep for the remaining time
         ros::spinOnce();
